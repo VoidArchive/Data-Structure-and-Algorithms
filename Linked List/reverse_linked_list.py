e754ee17fada5caaf7bf6ac3396547541bc6bc04
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-05-03 10:46:34 +0545

    add linked list

---
+++ Linked List/reverse_linked_list.py
@@ -0,0 +1,40 @@
+'''
+Difficulty: Easy
+https://leetcode.com/problems/reverse-linked-list/
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+'''
+
+
+
+class Solution:
+
+
+    # iteratively
+    def reverseList(self, head):
+        
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+        
+    # Recursively
+    def reverseList_recusive(self,head):
+
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList_recusive(head.next)
+            head.next.next = head
+
+        head.next = None    
+
+        return newHead
+

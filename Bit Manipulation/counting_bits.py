808e795eb1a9aa35e109b9f055d24c5bfac10409
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-05-20 11:03:04 +0545

    counting bits

---
+++ Bit Manipulation/counting_bits.py
@@ -0,0 +1,20 @@
+'''
+Difficulty: Easy
+https://leetcode.com/problems/counting-bits/
+
+Given an integer n,
+return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ans[i] is the number of 1's in the binary representation of i.
+
+'''
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+            
+            dp[i] = 1 + dp[i - offset]
+        
+        return dp

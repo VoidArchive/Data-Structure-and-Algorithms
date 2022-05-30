60b50635a929e64156ad790f7b8a1dd0a85d0372
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-05-30 10:12:11 +0545

    two _ sum

---
+++ Array & Hashing/longest_consecutive_number.py
@@ -0,0 +1,23 @@
+'''
+Difficulty: Medium
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+'''
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if n-1 not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        
+        return longest

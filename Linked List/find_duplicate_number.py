14f290714610e392283798d332dd28b2cdea25c3
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-06-19 10:26:53 +0545

    find duplicate number

---
+++ Linked List/find_duplicate_number.py
@@ -0,0 +1,33 @@
+'''
+Difficulty: Medium
+
+https://leetcode.com/problems/find-the-duplicate-number/
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+'''
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0 , 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+
+    
+

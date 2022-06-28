1eea334c6d2b398dea2318c896c26b37af9f7913
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-06-28 10:02:36 +0545

    trie

---
+++ Trie/implement_trie.py
@@ -0,0 +1,64 @@
+'''
+Difficulty: Medium
+
+
+https://leetcode.com/problems/implement-trie-prefix-tree/
+
+
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+'''
+
+
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.endOfWord = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+
+        return True
+
+        # Your Trie object will be instantiated and called as such:
+        # obj = Trie()
+        # obj.insert(word)
+        # param_2 = obj.search(word)
+        # param_3 = obj.startsWith(prefix)

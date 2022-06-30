2eee894bce502e9dc2351b830beeb8e53c7cab44
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-06-30 09:59:18 +0545

    kth closest point to origin

---
+++ Heap/k_closest_point_to_origin.py
@@ -0,0 +1,33 @@
+'''
+Difficulty: Medium
+
+https://leetcode.com/problems/k-closest-points-to-origin/
+
+
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+ 
+'''
+
+import heapq
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x**2) + (y**2)
+            minHeap.append([dist, x, y])
+
+        heapq.heapify(minHeap)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k -= 1
+
+        return res
